378f48b31e7d82f01ebcffc900b8b1fc7285d12d
Ganna <annakovalenko168@gmail.com>
2021-08-02 23:22:20 +0300

    1

--- src/js/task.js
+++ src/js/task.js
@@ -1,4 +1,4 @@
-import {galleryItems} from './app';
+import {galleryItems} from '../js/app.js';
 
 // Создание и рендер разметки по массиву данных galleryItems из app.js и предоставленному шаблону.
 // Реализация делегирования на галерее ul.js-gallery и получение url большого изображения.
